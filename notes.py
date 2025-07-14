f94a2a130394803eaf9fb47b0d6f4ae08615cb38
Felix Reiter <sbody1113@gmail.com>
2025-07-14 18:29:46 +0200

    added "exit" and "new" commands,changed some cmd logic

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
 import colorama as c
+import os
 def main():
     print("Programm Start...")
     c.init()
@@ -10,14 +11,25 @@ def main():
         print("~[" + c.Fore.GREEN + user + c.Fore.LIGHTMAGENTA_EX + "]$ " + c.Fore.YELLOW,end='')
         command = input()
         cmd = command.split(' ')
-        cmd = [i.lower().strip() for i in cmd]
-        op = cmd[0]
+        op = cmd[0].lower()
         print(c.Fore.CYAN)
         if op == "help":
 
             print("Liste der Befehle:")
             print("-- new NAME")
-            print("-- load MASTERNAME")
+            print("-- link MASTERNAME")
             print("-- add FILENAME")
+            print("-- addraw TEXT")
+            print("-- enc PASSWORD")  #TODO: Maybe change the password system so it is censored while the user is typing(like just enc and then later then password using idk password.input or smth
+            print("-- drop")
+            print("-- exit")
+        if op == "exit":
+            print("Auf wiedersehen!")
+            break
+        if op == "new":
+            name = cmd[1]
+            with open(f'{name}.dbk','w') as new:  #maybe we will be changing extension idk
+                new.write('0')
+    input("Drücke enter um das programm zu Schließen...")
 if __name__ == "__main__":
     main()
