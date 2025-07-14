da234589f85676df32dd963adcc8f27651e4d77c
Felix Reiter <sbody1113@gmail.com>
2025-07-14 18:07:52 +0200

    Earliest Runnable Version

--- notes.py
+++ notes.py
@@ -1,4 +1,13 @@
+import colorama as c
 def main():
     print("Programm Start...")
+    c.init()
+    print("Dieses Programm ist Konsolen basiert und benutzt eine prompt um deine entscheidungen auszuführen.")
+    print("Benutze den Befehl 'help' um Hilfe zu erhalten")
+    user = input("Benutzer?   ")
+    while True:
+        print(c.Style.RESET_ALL,end='')
+        print("~[" + user + "]$ ",end=c.Fore.YELLOW)
+        command = input() #add logic later
 if __name__ == "__main__":
     main()
