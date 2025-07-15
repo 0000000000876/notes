5fa750f28a30b3ad088343d37b9624c9e6eedfa5
Felix Reiter <sbody1113@gmail.com>
2025-07-15 17:52:38 +0200

    enc now ready

--- enc.py
+++ enc.py
@@ -1,4 +1,9 @@
 #encryption alg here
-def enc(text,pw):pass
+def enc(text,pw):
+    e_txt = ""
+    for i in range(len(text)):
+        e_txt += chr(ord(text[i]) ^ ord(pw[i % len(pw)]))
+    return e_txt
 
-def dec(text,pw):pass
+def dec(text,pw):
+    return enc(text,pw)
